322db66ae01a08a084f689015cff6ddd7ce1bcf7
Henrique Domingos <henrique-grupow@bitbucket.org>
2018-01-09 13:52:57 -0200

    Init

---
+++ init.js
@@ -0,0 +1,29 @@
+(function(){
+    var polo = {
+        sell_orders:{
+            exibir_total: exibir_total_sell_orders
+        }
+    }
+    window.polo_control = polo;
+
+
+
+
+    function exibir_total_sell_orders(){
+        var clone = $('.sellOrders.mainBox .info').clone();
+
+        clone.css('display', 'block')
+             .css('clear', 'both')
+             .css('margin-top', '-33px');
+
+        $('.currency', clone).html( $('.buyOrders .currency').html() );
+        
+        $('.sellOrders.mainBox .num').on('DOMSubtreeModified', function(){
+            $('.num', clone).html( (parseFloat($('.sellOrders.mainBox .num').html()) * parseFloat($('#hilights .lastPrice .info').html())).toFixed(8) );
+        });
+    
+        clone.appendTo($('.sellOrders.mainBox .info').parent());
+    
+    
+    };
+})();
